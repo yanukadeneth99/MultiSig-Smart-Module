6067d6084df2c6457125ef7cd96560af492e3ff4
YASHURA <61384503+yanukadeneth99@users.noreply.github.com>
2022-12-04 22:02:26 +0530

    Wrote multisig create functions

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -23,6 +23,15 @@ contract MultiSig is IMultiSig, IHyperverseModule, Initializable {
     //TODO : Change this
     bytes4 private constant _INTERFACE_ID = 0x0342a3c8;
 
+    // User -> Vault ID. Always starts with 1
+    mapping(address => uint256) private _vaultId;
+
+    // Holds the current vaults created
+    uint256 private _numOfVaults;
+
+    // Vault ID -> Vault Object
+    mapping(uint256 => Vault) private _vaults;
+
     /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ M O D I F I E R S @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
 
     // Is the caller the Tenant Owner
@@ -52,6 +61,14 @@ contract MultiSig is IMultiSig, IHyperverseModule, Initializable {
         _;
     }
 
+    // Check if there is an Address 0 in an array of addresses
+    modifier addressArrayCheck(address[] calldata _in) {
+        for (uint i = 0; i < _in.length; i++) {
+            if (_in[i] == address(0)) revert ZeroAddress();
+        }
+        _;
+    }
+
     /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ C O N S T R U C T O R @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
 
     /// @dev Make sure to update the information in the metadata before you deploy
@@ -66,7 +83,7 @@ contract MultiSig is IMultiSig, IHyperverseModule, Initializable {
         contractOwner = _owner;
     }
 
-    /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ F U N C T I O N S @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+    /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@ SETTER - F U N C T I O N S @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
 
     /// @dev Initializes the instance of a tenant for this contract and sets the state variables
     /// @param _tenant The address of the instance owner
@@ -76,5 +93,65 @@ contract MultiSig is IMultiSig, IHyperverseModule, Initializable {
         _tenantOwner = _tenant;
     }
 
-    ///+functions
+    /// @dev Creates a Vault with multiple users only
+    /// @param _userAddresses All the addresses you wish to add as users
+    function createVault(
+        address[] calldata _userAddresses
+    ) external addressArrayCheck(_userAddresses) {
+        // Create empty Vault Object
+        Vault memory _v;
+
+        // Create a new array which contains the owner
+        address[] memory _ownerSave = new address[](1);
+        _ownerSave[0] = msg.sender;
+
+        // Set the Owners to just the caller
+        _v.owners = _ownerSave;
+
+        // Set the users with the passed in address list of users
+        _v.users = _userAddresses;
+
+        // Votes will start with one since there is only one owner
+        _v.votes = 1;
+
+        // Set the Vault and increment the number of Vaults
+        _vaults[++_numOfVaults] = _v;
+    }
+
+    /// @dev Creates a Vault with multiple users and multiple owners
+    /// @param _userAddresses All the user addresses you wish to add
+    /// @param _ownerAddresses All the owner addresses you wish to add
+    function createVault(
+        address[] calldata _userAddresses,
+        address[] calldata _ownerAddresses
+    )
+        external
+        addressArrayCheck(_userAddresses)
+        addressArrayCheck(_ownerAddresses)
+    {
+        // Create empty Vault Object
+        Vault memory _v;
+
+        // Set the owners
+        _v.owners = _ownerAddresses;
+
+        // Set the users with the passed in address list of users
+        _v.users = _userAddresses;
+
+        // Votes will be initally the length of the number of owners
+        _v.votes = _ownerAddresses.length;
+
+        // Set the Vault and increment the number of Vaults
+        _vaults[++_numOfVaults] = _v;
+    }
+
+    /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@ GETTER - F U N C T I O N S @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+
+    /// @dev Gets the Vault Number of the caller if it exists, else reverts
+    function getVault() external view returns (uint256) {
+        uint256 _val = _vaultId[msg.sender];
+        if (_val == uint256(0)) revert AddressNotInAVault();
+
+        return _val;
+    }
 }
--- contracts/interface/IMultiSig.sol
+++ contracts/interface/IMultiSig.sol
@@ -6,9 +6,34 @@ interface IMultiSig {
 
     ///+events
 
+    /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ S T R U C T S @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+
+    /// @dev Vault Object holds all the values required to create and manage a vault
+    /// @dev Owners and Users will have specific permissions
+    /// @dev Votes are the total votes required to pass a transaction (counted from the number of owners)
+    struct Vault {
+        address[] owners;
+        address[] users;
+        uint256 votes;
+    }
+
     /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ E R R O R S @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+
+    // Triggered when a function is access which the user has no permission to
     error Unauthorized();
+
+    // Trigerred when a functionality is called on an already initialised contract
     error AlreadyInitialized();
+
+    // Triggered when a function is being used with an Address 0
     error ZeroAddress();
+
+    // Triggered when a functionality is requested using the same `to` and `from` address
     error SameAddress();
+
+    // Triggered when there is no vault
+    error InvalidVault();
+
+    // Triggered when a functionality is done on an address that does not belong to any vault
+    error AddressNotInAVault();
 }
